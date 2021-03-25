79c4c5dabe8f98e53b60669802f3650682bf2cf7
misuko1999 <hhq85@gmail.com>
2021-03-25 23:45:43 +0700

    UI Header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter, Redirect, Route, Switch, Link } from 'react-router-dom';
 import './App.scss';
+import Header from './components/Header';
 import NotFound from './components/NotFound';
 
 
@@ -11,20 +12,16 @@ function App() {
 	return (
 		<div className='App'>
 			<Suspense fallback={<div>Loading...</div>}>
-        <BrowserRouter>
-          <ul>
-            <li><Link to="/photos">Go to photo page</Link></li>
-            <li><Link to="/photos/add">Go to Add new photo page</Link></li>
-            <li><Link to="/photos/123">Go to Edit photo page</Link></li>
-          </ul>
-          <Switch>
-            <Redirect exact from='/' to='/photos' />
-            <Route path='/photos' component={Photo}/>
-            <Route component={NotFound} />
+				<BrowserRouter>
+					<Header   />
+					<Switch>
+						<Redirect exact from='/' to='/photos' />
 
-          </Switch>
-        </BrowserRouter>
-      </Suspense>
+						<Route path='/photos' component={Photo} />
+						<Route component={NotFound} />
+					</Switch>
+				</BrowserRouter>
+			</Suspense>
 		</div>
 	);
 }
